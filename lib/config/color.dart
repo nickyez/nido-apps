3e2f9b4743522cfd718ff250e4fce3e376ae2d25
NickyErlanggaZ <nickyerlanggaz@gmail.com>
2021-12-02 12:40:48 +0700

    Menambahkan konfigurasi warna

---
+++ lib/config/color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// penamaan kode hex (0xFF + 'kode hex color')
+// misalkan kode color #0F1113, menjadi 0xFF0F1113
+class ColorApps{
+  static const Color primaryColor = Color(0xFF0EBAB0);
+  static const Color white = Color(0xFFFFF8E8);
+  static const Color menuAngka = Color(0xFFEB6980);
+  static const Color menuBentuk = Color(0xFFF7BD2E);
+  static const Color menuWarna = Color(0xFF6854A4);
+  static const Color menuHuruf = Color(0xFF008AC0);
+}
