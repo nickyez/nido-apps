59ec99043d3eabd1d0f807bda014861e44c7f18c
NickyErlanggaZ <nickyerlanggaz@gmail.com>
2021-12-19 10:14:19 +0700

    add: models

---
+++ lib/models/bentuk.dart
@@ -0,0 +1,34 @@
+class Bentuk {
+  static const List<String> imageUrl = [
+    "assets/images/bentuk/ic_bentuk_1.png",
+    "assets/images/bentuk/ic_bentuk_2.png",
+    "assets/images/bentuk/ic_bentuk_3.png",
+    "assets/images/bentuk/ic_bentuk_4.png",
+    "assets/images/bentuk/ic_bentuk_5.png",
+    "assets/images/bentuk/ic_bentuk_6.png"
+  ];
+  static const List<String> name = [
+    "Segitiga",
+    "Bintang",
+    "Lingkaran",
+    "Trapesium",
+    "Persegi",
+    "Segi Enam"
+  ];
+  static const List<String> shape = [
+    "assets/images/bentuk/ic_bentuk_triangle.png",
+    "assets/images/bentuk/ic_bentuk_star.png",
+    "assets/images/bentuk/ic_bentuk_circle.png",
+    "assets/images/bentuk/ic_bentuk_4.png",
+    "assets/images/bentuk/ic_bentuk_6.png",
+    "assets/images/bentuk/ic_bentuk_3.png",
+  ];
+  static const List<String> shape2 = [
+    "assets/images/bentuk/ic_bentuk_2.png",
+    "assets/images/bentuk/ic_bentuk_1.png",
+    "assets/images/bentuk/ic_bentuk_5.png",
+    "assets/images/bentuk/ic_bentuk_4.png",
+    "assets/images/bentuk/ic_bentuk_6.png",
+    "assets/images/bentuk/ic_bentuk_3.png",
+  ];
+}
---
+++ lib/models/warna.dart
@@ -0,0 +1,26 @@
+import 'dart:ui';
+
+class Warna{
+  static const List<String> imageUrl = [
+    "assets/images/warna/ic_warna_1.png",
+    "assets/images/warna/ic_warna_2.png",
+    "assets/images/warna/ic_warna_3.png",
+    "assets/images/warna/ic_warna_4.png",
+    "assets/images/warna/ic_warna_5.png",
+    "assets/images/warna/ic_warna_6.png"
+  ];
+  static const List<String> name = [
+    "Oranye",
+    "Hijau",
+    "Biru",
+    "Merah",
+    "Ungu",
+    "Kuning"
+  ];
+  static const List<Color> color = [
+    Color(0xFFF47D3D), // Oranye
+    Color(0xFF79C35E), // Hijau
+    Color(0xFF55C5CE), // Biru
+    Color(0xFFDF3040), // Merah
+  ];
+}
